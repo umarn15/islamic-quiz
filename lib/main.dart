f9eea3d253204e69305e5924ae87a6140276ea1d
Umar <umern9947@gmail.com>
2025-12-26 17:48:29 +0500

    fix gradle issue

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+      ),
     );
   }
 }
